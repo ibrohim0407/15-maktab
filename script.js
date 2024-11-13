5c9400cbe67382dc07011274cfd9870542163935
ibrohim0407 <132291535+ibrohim0407@users.noreply.github.com>
2024-11-13 22:21:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-document.getElementById("logintap").onclick = function(){
-  let x;
-  x=document.getElementById("username").value;
-  let y;
-  y=document.getElementById("password").value;
-  console.log(x + ": " + y);
+function parolmatch(usernames,password,user,pass){
+  let ans=false;
+  
+}
+document.getElementById('logintap').onclick = function(){
+  let usernames; 
+  let passwords;
+  fetch('usernames.txt')
+        .then(response => response.text())
+        .then(data => {
+            // Split the content by lines into an array
+            usernames = data.split('\n');
+        })
+        .catch(error => console.error('Error fetching the file:', error));
+  fetch('passwords.txt')
+        .then(response => response.text())
+        .then(data => {
+            passwords = data.split('\n');
+        })
+        .catch(error => console.error('Error fetching the file:', error));
+  user=document.getElementById("username").value;
+  pass=document.getElementById("password").value;
+  if(parolmatch(usernames,passwords,user,pass)){
+    console.log("true password");
+  }
+  else{
+    console.log("bad passoword")
+  }
+  
 }
