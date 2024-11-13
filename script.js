8818bb99eb1dc5d943b7adbf430e8220a83f294c
ibrohim0407 <132291535+ibrohim0407@users.noreply.github.com>
2024-11-13 10:24:40 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("logintap").onclick = function(){
+  let x;
+  x=document.getElementById("username").value;
+  let y;
+  y=document.getElementById("password").value;
+  console.log(x + ": " + y);
+}
